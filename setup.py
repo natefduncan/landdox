a1f8ddfc46e7cde2ec2dad77ba25cd41969ee31f
natefduncan <natefduncan@gmail.com>
2019-12-12 14:47:07 -0600

    Update requirements and setup

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     url = 'https://github.com/natefduncan/landdox',
     download_url = 'https://github.com/natefduncan/landdox/archive/1.0.tar.gz',
     keywords = ['landdox', 'oil'],
-    install_requires=['requests'],
+    install_requires=['requests', 'pandas'],
     classifiers = [
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" 
     'Intended Audience :: Developers',     
